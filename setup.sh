9b884716556b2bb8143355114ddd0024ef527397
Masaya Kataoka <ms.kataoka@gmail.com>
2021-10-09 22:58:31 +0900

    add setup_envrionment_variable script
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 cd ansible
 sh setup_ansible.sh
 cd ../
-ansible-playbook -i ansible/hosts/localhost.ini ansible/setup_dev_environment.yml --vars ansible/vars/github_token.yaml --connection local --ask-become-pass --ask-vault-pass
+ansible-playbook -i ansible/hosts/localhost.ini ansible/setup_dev_environment.yml --connection local --ask-become-pass
