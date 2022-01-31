c4fff602a53e1f5e7324e14eb36da30281f8e21f
MasayaKataoka <ms.kataoka@gmail.com>
2022-02-01 01:21:34 +0900

    modify role
    Signed-off-by: MasayaKataoka <ms.kataoka@gmail.com>

--- ansible/setup_ansible.sh
+++ ansible/setup_ansible.sh
@@ -1,5 +1,4 @@
 sudo apt update
 sudo apt install python3 python3-pip
 sudo pip install ansible\==2.9.8
-ansible-galaxy install monolithprojects.github_actions_runner -p roles/github_actions_runner
-ansible-galaxy install geerlingguy.nodejs -p roles/nodejs
+ansible-galaxy install monolithprojects.github_actions_runner
