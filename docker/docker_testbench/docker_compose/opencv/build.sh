9f261699b2d3fed81ab9215670f1de947fc11ced
MasayaKataoka <ms.kataoka@gmail.com>
2022-03-06 23:21:47 +0900

    modify compile options
    Signed-off-by: MasayaKataoka <ms.kataoka@gmail.com>

--- docker/docker_testbench/docker_compose/opencv/build.sh
+++ docker/docker_testbench/docker_compose/opencv/build.sh
@@ -3,22 +3,25 @@
 
 cd build && \
 cmake \
-    -D CPACK_BINARY_DEB=ON \
     -D BUILD_EXAMPLES=OFF \
+    -D BUILD_opencv_cudev=ON \
+    -D BUILD_opencv_java=OFF \
     -D BUILD_opencv_python2=OFF \
     -D BUILD_opencv_python3=ON \
-    -D BUILD_opencv_java=OFF \
     -D BUILD_opencv_world=OFF \
-    -D BUILD_opencv_cudev=ON \
+    -D BUILD_PERF_TESTS=OFF \
+    -D BUILD_TESTS=OFF \
+    -D BUILD_TIFF=ON \
     -D CMAKE_BUILD_TYPE=RELEASE \
     -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D CPACK_BINARY_DEB=ON \
     -D CUDA_ARCH_BIN=5.3,6.2,7.2 \
     -D CUDA_ARCH_PTX= \
     -D CUDA_FAST_MATH=ON \
     -D CUDNN_INCLUDE_DIR=/usr/include/aarch64-linux-gnu \
     -D EIGEN_INCLUDE_PATH=/usr/include/eigen3 \
-    -D WITH_EIGEN=ON \
     -D ENABLE_NEON=ON \
+    -D INSTALL_C_EXAMPLES=OFF \
     -D OPENCV_DNN_CUDA=ON \
     -D OPENCV_ENABLE_NONFREE=ON \
     -D OPENCV_EXTRA_MODULES_PATH=/home/ubuntu/opencv_contrib/modules \
@@ -26,15 +29,13 @@ cmake \
     -D WITH_CUBLAS=ON \
     -D WITH_CUDA=ON \
     -D WITH_CUDNN=ON \
+    -D WITH_EIGEN=ON \
     -D WITH_GSTREAMER=ON \
-    -D WITH_V4L=ON \
-    -D WITH_OPENGL=ON \
-    -D WITH_OPENCL=OFF \
     -D WITH_IPP=OFF \
+    -D WITH_OPENCL=OFF \
+    -D WITH_OPENGL=ON \
     -D WITH_TBB=ON \
-    -D BUILD_TIFF=ON \
-    -D BUILD_PERF_TESTS=OFF \
-    -D BUILD_TESTS=OFF \
+    -D WITH_V4L=ON \
 /home/ubuntu/opencv
 
 make -j$(nproc)
