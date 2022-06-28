cc93872d7590aa93fb727ceb954080a9c87d8db6
Masaya Kataoka <ms.kataoka@gmail.com>
2022-06-28 20:08:57 +0900

    add roles for setting up ml pipeline
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 cd ansible
 sh setup_ansible.sh
 cd ../
+ansible-galaxy install -fr roles/requirements.yml
 ansible-playbook -i ansible/hosts/localhost.ini ansible/setup_dev_environment.yml --connection local --ask-become-pass
