3aed40107ae92259d5d72333086802566b98c8e7
Masaya Kataoka <ms.kataoka@gmail.com>
2022-07-15 00:39:39 +0900

    add manifest
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- ml_pipeline/start_with_minikube.sh
+++ ml_pipeline/start_with_minikube.sh
@@ -1,3 +1,5 @@
 minikube start
 kubectl create ns argo
 kubectl apply -n argo -f https://raw.githubusercontent.com/argoproj/argo/stable/manifests/quick-start-postgres.yaml
+kubectl create ns minio-trained-weights
+kubectl apply -f manifests/minio-trained-weights.yaml
