4d0035dc32c48c81cd5c84e0922f5f06fd54b0f8
Yusuke Mizoguchi <33048729+swiftfile@users.noreply.github.com>
2022-07-23 17:29:43 +0900

    fix path to requirements.yml (#236)

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 cd ansible
 sh setup_ansible.sh
 cd ../
-ansible-galaxy install -fr roles/requirements.yml
+ansible-galaxy install -fr ansible/roles/requirements.yml
 ansible-playbook -i ansible/hosts/localhost.ini ansible/setup_dev_environment.yml --connection local --ask-become-pass
