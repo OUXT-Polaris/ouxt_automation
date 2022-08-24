d57c20a11e2893f6a4199a6f848452f0f6f4504b
Masaya Kataoka <ms.kataoka@gmail.com>
2022-08-25 02:14:54 +0900

    update docker
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- .github/workflows/docker/yolox_trainer/entrypoint.sh
+++ .github/workflows/docker/yolox_trainer/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -l
 cd /home/runner/actions-runner
 curl -u wamvtan:$GITHUB_TOKEN -X POST -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/OUXT-Polaris/dataset_annotations/actions/runners/registration-token | jq '.token' | tr -d \" | { read v ;./config.sh --url https://github.com/OUXT-Polaris/dataset_annotations --token "$v" --labels yolox_trainer --ephemeral;}
-
-./run.sh --ephemeral
+./run.sh
