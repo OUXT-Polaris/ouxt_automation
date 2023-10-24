8a6f1322afa2f635dc66de96af8bd27cd9e1f124
Masaya Kataoka <ms.kataoka@gmail.com>
2023-10-25 00:05:13 +0900

    use builtin module
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- setup_uav.sh
+++ setup_uav.sh
@@ -1,5 +1,4 @@
 cd ansible
 sh setup_ansible.sh
 cd ../
-ansible-galaxy install -fr ansible/roles/requirements.yml
 ansible-playbook -i ansible/hosts/uav.ini ansible/setup_uav.yml --connection ssh --ask-become-pass
