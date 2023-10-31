42f1cb68b79855ba70ad6950f31d5d66824b920d
Kenta Okamoto <kenntabsk@gmail.com>
2023-10-31 23:12:14 +0900

    fix dockerfiles

--- .github/workflows/docker/vrx/run_vrx_gazebo.bash
+++ .github/workflows/docker/vrx/run_vrx_gazebo.bash
@@ -2,4 +2,4 @@
 ros2 launch vrx_gazebo generate_wamv.launch.py component_yaml:=/home/config/component_config.yaml thruster_yaml:=/home/config/thruster_config.yaml wamv_target:=/home/config/wamv_target.urdf wamv_locked:=False
 
 # launch vrx gazebo
-ros2 launch vrx_gz competition.launch.py world:=sydney_regatta headless:=true urdf:=/home/config/wamv_target.urdf
+# ros2 launch vrx_gz competition.launch.py world:=stationkeeping_task headless:=true urdf:=/home/config/wamv_target.urdf
