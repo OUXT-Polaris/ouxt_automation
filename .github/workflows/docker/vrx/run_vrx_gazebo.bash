4d88a2754fc0d5bffec223b498b3629ff9d0ba7e
NaNnaru <63537140+kentaokamoto@users.noreply.github.com>
2023-11-02 00:09:08 +0900

    Update run_vrx_gazebo.bash
    add command to run vrx_bridge
    # run vrx_bridge node to exchange sensor info with nav_sim
    ros2 run vrx_bridge vrx_bridge_node&

--- .github/workflows/docker/vrx/run_vrx_gazebo.bash
+++ .github/workflows/docker/vrx/run_vrx_gazebo.bash
@@ -1,5 +1,8 @@
 # generate wamv_target.urdf
 ros2 launch vrx_gazebo generate_wamv.launch.py component_yaml:=/home/config/component_config.yaml thruster_yaml:=/home/config/thruster_config.yaml wamv_target:=/home/config/wamv_target.urdf wamv_locked:=False
 
+# run vrx_bridge node to exchange sensor info with nav_sim
+ros2 run vrx_bridge vrx_bridge_node&
+
 # launch vrx gazebo
 # ros2 launch vrx_gz competition.launch.py world:=stationkeeping_task headless:=true urdf:=/home/config/wamv_target.urdf
