8476d449aadeb0bbcd2771a6037267fb4f365fa8
Masaya Kataoka <ms.kataoka@gmail.com>
2024-02-23 00:33:12 +0900

    Feature/auto logger (#331)
    * add docker build action
    
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>
    
    * rename docker
    
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>
    
    * enable logging all topics
    
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>
    
    * add rosbag directory
    
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>
    
    * update entrypoint
    
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>
    
    ---------
    
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

---
+++ .github/workflows/docker/auto_logger/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+# setup ros environment.
+source "/opt/ros/$ROS_DISTRO/setup.bash"
+
+# exec "$@"
+
+# record all topics
+ros2 bag record -a -d 10 -s mcap -o /rosbag/$(date '+%Y-%m-%d-%H-%M-%S')
