096e7240a77c773a773d7965b1f4cab1e5a381e0
Masaya Kataoka <ms.kataoka@gmail.com>
2024-04-11 22:49:56 +0900

    remvoe requirements.yml
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,4 +1,3 @@
 sh install_poetry.sh
 poetry install
-poetry run ansible-galaxy install -fr ansible/roles/requirements.yml
 poetry run ansible-playbook -i ansible/hosts/localhost.ini ansible/setup_dev_environment.yml --connection local --ask-become-pass -e ansible_user=${USER}
--- setup_uav.sh
+++ setup_uav.sh
@@ -1,4 +1,3 @@
 sh install_poetry.sh
 poetry install
-poetry run ansible-galaxy install -fr ansible/roles/requirements.yml
 poetry run ansible-playbook -i ansible/hosts/uav.ini ansible/setup_uav.yml --connection ssh --ask-become-pass
