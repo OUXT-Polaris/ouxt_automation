963e14f49ff289b340eb3bb98c6c8b983669e68d
Masaya Kataoka <ms.kataoka@gmail.com>
2024-04-11 23:56:02 +0900

    update setup script
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 sh install_poetry.sh
+sudo apt update
+sudo apt install -y python-apt-dev
 poetry install
 poetry run ansible-playbook -i ansible/hosts/localhost.ini ansible/setup_dev_environment.yml --connection local --ask-become-pass -e ansible_user=${USER}
