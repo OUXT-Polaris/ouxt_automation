2fa2be9a4977a171d2934e4065d71c71bcb11e54
Masaya Kataoka <ms.kataoka@gmail.com>
2024-09-29 18:53:06 +0900

    add Docker Image for kicad
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

---
+++ circuits/start_kicad.sh
@@ -0,0 +1,10 @@
+docker run \
+  --rm \
+  --name=kicad \
+  --security-opt seccomp=unconfined `#optional` \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e TZ=Etc/UTC \
+  -p 3000:3000 \
+  -p 3001:3001 \
+  wamvtan/kicad:latest
