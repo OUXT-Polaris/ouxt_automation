73d66f8ae88744daa975b9aa4e65ec9a617ec75a
Masaya Kataoka <ms.kataoka@gmail.com>
2024-10-05 23:02:54 +0900

    enable use x11 forwarding
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- circuits/start_kicad.sh
+++ circuits/start_kicad.sh
@@ -1,11 +1,13 @@
-docker run \
-  --rm \
-  --name=kicad \
-  --security-opt seccomp=unconfined `#optional` \
-  -e PUID=1000 \
-  -e PGID=1000 \
-  -e TZ=Etc/UTC \
-  -p 3000:3000 \
-  -p 3001:3001 \
-  -v ${PWD}/computer_sensor_board:/config/circuits/computer_sensor_board \
-  wamvtan/kicad:latest
+# docker run \
+#   --rm \
+#   --name=kicad \
+#   --security-opt seccomp=unconfined `#optional` \
+#   -e PUID=1000 \
+#   -e PGID=1000 \
+#   -e TZ=Etc/UTC \
+#   -p 3000:3000 \
+#   -p 3001:3001 \
+#   -v ${PWD}/computer_sensor_board:/config/circuits/computer_sensor_board \
+#   wamvtan/kicad:latest
+
+docker run -e DISPLAY=$DISPLAY --net=host --rm wamvtan/kicad:latest
