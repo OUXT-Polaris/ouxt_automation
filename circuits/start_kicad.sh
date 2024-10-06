cd14c16bfb2d72be62681a079d26e74f1dd45b52
Masaya Kataoka <ms.kataoka@gmail.com>
2024-10-06 22:27:43 +0900

    add entrypoint.sh
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- circuits/start_kicad.sh
+++ circuits/start_kicad.sh
@@ -4,4 +4,4 @@ docker run --name=kicad \
   --rm \
   -v ${PWD}/computer_sensor_board:/workspace/computer_sensor_board \
   -v ${PWD}/3rdparty:/root/.local/share/kicad/8.0/3rdparty \
-  wamvtan/kicad:latest
+  wamvtan/kicad:latest kicad
