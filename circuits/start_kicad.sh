e552d34d940701dcf04b2f916874677ff4daced9
Masaya Kataoka <ms.kataoka@gmail.com>
2024-10-13 21:50:04 +0900

    add teensy library
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- circuits/start_kicad.sh
+++ circuits/start_kicad.sh
@@ -1,7 +1,8 @@
 docker run --name=kicad \
+  -it \
   -e DISPLAY=$DISPLAY \
   --net=host \
   --rm \
   -v ${PWD}/computer_sensor_board:/workspace/computer_sensor_board \
   -v ${PWD}/3rdparty:/root/.local/share/kicad/8.0/3rdparty \
-  wamvtan/kicad:latest kicad
+  wamvtan/kicad:latest /bin/bash
