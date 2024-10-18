a1f45a6ffea6b802bdfc96fc080b1c7bcc9cf318
Masaya Kataoka <ms.kataoka@gmail.com>
2024-10-19 08:36:02 +0900

    update .gitignore
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

---
+++ circuits/3rdparty/plugins/org_openscopeproject_InteractiveHtmlBom/web/user.js
@@ -0,0 +1,6 @@
+// EventHandler.registerCallback(IBOM_EVENT_TYPES.BOM_BODY_CHANGE_EVENT, () => {
+//     for(var tr of bom.childNodes) {
+//         tr.onclick = tr.onmousemove;
+//         tr.onmousemove = null;
+//     };
+// });
