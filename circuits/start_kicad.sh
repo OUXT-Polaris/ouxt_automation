023284341208ca8e5c3caae16245e9afb04dba88
Masaya Kataoka <ms.kataoka@gmail.com>
2024-12-01 14:11:03 +0900

    Feature/motor controller board (#363)
    * add miniv_motor_controller_board
    
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>
    
    * add connectors
    
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>
    
    * add some footprint
    
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>
    
    * update connection
    
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>
    
    * update schematic
    
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>
    
    * add PCBGOGO Plugin
    
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>
    
    * add mounting hole
    
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>
    
    * enable generate gerber
    
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>
    
    * enable generate drill file
    
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>
    
    * modify resister
    
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>
    
    * fix connection
    
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>
    
    * update text
    
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>
    
    * add hole
    
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>
    
    * add RJ45 connector
    
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>
    
    * add footprint
    
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>
    
    * add SS-12D00G3 and modify RJLD-260YC1
    
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>
    
    * update board
    
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>
    
    * modify circuit
    
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>
    
    * add 12V DC/DC
    
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>
    
    * update schematic
    
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>
    
    * re-design board
    
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>
    
    * remove R1/R2
    
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>
    
    * change line width
    
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>
    
    * modify board
    
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>
    
    * remove bom
    
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>
    
    * add R1/R2
    
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>
    
    * update document
    
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>
    
    ---------
    
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- circuits/start_kicad.sh
+++ circuits/start_kicad.sh
@@ -4,5 +4,6 @@ docker run --name=kicad \
   --net=host \
   --rm \
   -v ${PWD}/computer_sensor_board:/workspace/computer_sensor_board \
+  -v ${PWD}/miniv_motor_controller_board:/workspace/miniv_motor_controller_board \
   -v ${PWD}/3rdparty:/root/.local/share/kicad/8.0/3rdparty \
   wamvtan/kicad:latest kicad
