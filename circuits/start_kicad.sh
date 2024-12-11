daede76f108ca334209f641df5f1f36b1411985b
Masaya Kataoka <ms.kataoka@gmail.com>
2024-12-11 09:24:01 +0900

    modify script
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- circuits/start_kicad.sh
+++ circuits/start_kicad.sh
@@ -5,5 +5,6 @@ docker run --name=kicad \
   --rm \
   -v ${PWD}/computer_sensor_board:/workspace/computer_sensor_board \
   -v ${PWD}/miniv_motor_controller_board:/workspace/miniv_motor_controller_board \
+  -v ${PWD}/miniv_smart_battery:/workspace/miniv_smart_battery \
   -v ${PWD}/3rdparty:/root/.local/share/kicad/8.0/3rdparty \
   wamvtan/kicad:latest kicad
