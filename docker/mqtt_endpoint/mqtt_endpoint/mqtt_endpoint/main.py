b0922ed13de2f3440d0dd64d36113051203e63ff
Masaya Kataoka <ms.kataoka@gmail.com>
2025-03-04 00:16:28 +0900

    add python script for mqtt heartbeat
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

---
+++ docker/mqtt_endpoint/mqtt_endpoint/mqtt_endpoint/main.py
@@ -0,0 +1,20 @@
+import time
+import paho.mqtt.client as mqtt
+
+def main():
+    broker = "54.212.20.15"
+    port = 1883
+    topic = "miniv/heartbeat"
+
+    client = mqtt.Client()
+
+    client.connect(broker, port)
+
+    while True:
+        message = "Hello, MQTT!"
+        print(f"Sending message: {message}")
+        client.publish(topic, message)
+        time.sleep(2)
+
+if __name__ == "__main__":
+    main()
