7ff9ee4d5b25141e3625f6e93237f04ee9dd068d
Masaya Kataoka <ms.kataoka@gmail.com>
2025-03-04 15:46:15 +0900

    add check last message function
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- .github/workflows/docker/mqtt_endpoint/mqtt_endpoint/mqtt_endpoint/ground_station_heartbeat.py
+++ .github/workflows/docker/mqtt_endpoint/mqtt_endpoint/mqtt_endpoint/ground_station_heartbeat.py
@@ -8,10 +8,23 @@ class GroundStationHeartBeat:
         self.topic = topic
         self.timeout = timeout
         self.is_timeout = False
+        self.is_emergency = False
         self.scheduler = scheduler
+        self.last_message = ""
+        self.autonomous_message = "AUTO"
+        self.emergency_message = "EMERGENCY"
         self.check_timeout(self.scheduler)
+        self.check_last_message(self.scheduler)
 
-    def receive(self):
+    def check_last_message(self, scheduler):
+        if self.last_message == self.emergency_message:
+            self.is_emergency = True
+        if self.last_message == self.autonomous_message:
+            self.is_emergency = False
+        scheduler.enter(0.1, 2, self.check_timeout, (scheduler,))
+
+    def receive(self, message):
+        self.last_message = message
         self.last_timestamp = datetime.datetime.now()
 
     def check_timeout(self, scheduler):
