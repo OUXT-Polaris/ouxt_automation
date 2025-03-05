3a030e50389cf609637cfc8042827780dc8ab76f
atomon <38009289+atomon@users.noreply.github.com>
2025-03-05 23:24:29 +0900

    feat: apply joy stick data

--- .github/workflows/docker/mqtt_endpoint/mqtt_endpoint/mqtt_endpoint/test.py
+++ .github/workflows/docker/mqtt_endpoint/mqtt_endpoint/mqtt_endpoint/test.py
@@ -6,6 +6,7 @@ from mqtt_endpoint.ground_station_heartbeat_pb2 import (
     ground_station_heartbeat,
 )
 import socket
+from joy_controller import JoyController
 
 
 def on_message(client, userdata, msg):
@@ -14,22 +15,37 @@ def on_message(client, userdata, msg):
 
 def main():
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    command = hardware_communication_msgs__MotorControl()
-    command.motor_enable = True
-    command.motor_speed = 0.3
-    command.mode = 1
+
+    command_l = hardware_communication_msgs__MotorControl()
+    command_r = hardware_communication_msgs__MotorControl()
+    command_l.motor_enable = True
+    command_r.motor_enable = True
+
     heartbeat_command = ground_station_heartbeat()
-    heartbeat_command.sequence = 1
-    heartbeat_command.mode = 1
+    sequence = 1
+    
+    joy_controller = JoyController()
+
     while True:
+        joy_controller.update()
+        
+        command_l.motor_speed = joy_controller.stick_ly
+        command_r.motor_speed = joy_controller.stick_ry
+        
+        command_l.mode = joy_controller.mode
+        command_r.mode = joy_controller.mode
+
+        heartbeat_command.sequence = sequence = sequence + 1
+        heartbeat_command.mode = joy_controller.mode
+        
         print("Pass")
         udp_socket.sendto(
-            hardware_communication_msgs__MotorControl.SerializeToString(command),
-            ("192.168.0.102", 8888),
+            hardware_communication_msgs__MotorControl.SerializeToString(command_r),
+            ("192.168.0.101", 8888),
         )
         udp_socket.sendto(
-            hardware_communication_msgs__MotorControl.SerializeToString(command),
-            ("192.168.0.101", 8888),
+            hardware_communication_msgs__MotorControl.SerializeToString(command_l),
+            ("192.168.0.102", 8888),
         )
         udp_socket.sendto(
             ground_station_heartbeat.SerializeToString(heartbeat_command),
