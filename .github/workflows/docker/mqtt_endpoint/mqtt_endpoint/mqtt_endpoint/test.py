31f90991d8807ae8c91bf5b3543b8ac9ae87e454
Masaya Kataoka <ms.kataoka@gmail.com>
2025-03-05 13:21:54 +0900

    update sample code
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- .github/workflows/docker/mqtt_endpoint/mqtt_endpoint/mqtt_endpoint/test.py
+++ .github/workflows/docker/mqtt_endpoint/mqtt_endpoint/mqtt_endpoint/test.py
@@ -5,20 +5,11 @@ from mqtt_endpoint.hardware_communication_msgs__MotorControl_pb2 import (
 from mqtt_endpoint.ground_station_heartbeat_pb2 import (
     ground_station_heartbeat,
 )
-import paho.mqtt.client as mqtt
 import socket
 
 
 def on_message(client, userdata, msg):
     pass
-
-
-def on_connect(client, userdata, flags, rc):
-    if rc == 0:
-        print("Connected to MQTT broker")
-        client.subscribe("#", qos=0)
-    else:
-        print(f"Connection failed with code {rc}")
 
 
 def main():
@@ -30,10 +21,6 @@ def main():
     heartbeat_command = ground_station_heartbeat()
     heartbeat_command.sequence = 1
     heartbeat_command.mode = 1
-    mqtt_client = mqtt.Client()
-    mqtt_client.connect("54.212.20.15", 1883, 3)
-    mqtt_client.on_message = on_message
-    mqtt_client.on_connect = on_connect
     while True:
         print("Pass")
         udp_socket.sendto(
@@ -48,4 +35,4 @@ def main():
             ground_station_heartbeat.SerializeToString(heartbeat_command),
             ("192.168.0.103", 4000),
         )
-        mqtt_client.loop_start()
+\\
