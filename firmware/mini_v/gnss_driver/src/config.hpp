85ba10d713dbe23e46ab29ad294cd27e36da94cb
atomon <38009289+atomon@users.noreply.github.com>
2025-03-08 00:42:17 +0900

    get imu data && send data with udp

--- firmware/mini_v/gnss_driver/src/config.hpp
+++ firmware/mini_v/gnss_driver/src/config.hpp
@@ -5,10 +5,12 @@
 // self
 byte MAC[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 const IPAddress IP(192, 168, 0, 100);
-const unsigned int LOCALPORT = 8888;
+const unsigned int LOCALPORT_GNSS = 8888;
+const unsigned int LOCALPORT_IMU = 8889;
 
 // destination
-const IPAddress IP_DIST(192, 168, 0, 200);
-const unsigned int LOCALPORT_DIST = 5000;
+const IPAddress IP_DIST(192, 168, 0, 50);
+const unsigned int LOCALPORT_GNSS_DIST = 5000;
+const unsigned int LOCALPORT_IMU_DIST = 5001;
 
 #endif
